1db080248b5040936716a5caa9ded79dca049e13
DrSumner <157843168+DrSumner@users.noreply.github.com>
2024-11-09 12:18:45 -0500

    clean up

--- src/pages/infopages/more.jsx
+++ src/pages/infopages/more.jsx
@@ -12,60 +12,15 @@ const More = (props) => {
             <h3 
             style={{color:'red'}}
             >**Warning not every song is clean**</h3>
-            <ul>
-                
-                <li>
+            
                 <iframe 
                 title="Songs Touched by Tre"
                 allow="autoplay *; encrypted-media *;" frameborder="0" height="450" 
                 style={{ width: "100%", maxWidth: "660px", overflow: "hidden", background: "transparent" }} 
                 sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" 
-                src="https://embed.music.apple.com/us/playlist/3-songs-touched-by-tr%C3%A9/pl.u-oZyleeaCRMPyN2P"></iframe>
-                </li>
-
-                {/* <li>
-                <iframe 
-                title="Need Myself - $oL"
-                allow="autoplay *; encrypted-media *;" frameborder="0" height="150" 
-                style={{ width: "100%", maxWidth: "660px", overflow: "hidden", background: "transparent" }} 
-                sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" 
-                src="https://embed.music.apple.com/us/album/need-myself/1601205213?i=1601205551">
+                src="https://embed.music.apple.com/us/playlist/3-songs-touched-by-tr%C3%A9/pl.u-oZyleeaCRMPyN2P">
                 </iframe>
-                </li>
 
-                <li>
-                <iframe 
-                title="Again - YBK"
-                allow="autoplay *; encrypted-media *;" frameborder="0" height="150" 
-                style={{ width: "100%", maxWidth: "660px", overflow: "hidden", background: "transparent" }} 
-                sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" 
-                src="https://embed.music.apple.com/us/album/again/1580432272?i=1580432277">
-                </iframe>
-                </li>
-
-                <li>
-                <iframe 
-                title="Over Again - IsthatAvo"
-                allow="autoplay *; encrypted-media *;" 
-                frameborder="0" height="150" 
-                style={{ width: "100%", maxWidth: "660px", overflow: "hidden", background: "transparent" }} 
-                sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" 
-                src="https://embed.music.apple.com/us/album/over-again/1535774552?i=1535774553">
-                </iframe>
-                </li>
-
-                <li>
-                <iframe 
-                title="The Resistance - Drake"
-                allow="autoplay *; encrypted-media *;" 
-                frameborder="0" 
-                height="150" 
-                style={{ width: "100%", maxWidth: "660px", overflow: "hidden", background: "transparent" }}
-                sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" 
-                src="https://embed.music.apple.com/us/album/the-resistance/1440762388?i=1440762512">
-                </iframe>
-                </li> */}
-            </ul>
         </div>
 
     );
