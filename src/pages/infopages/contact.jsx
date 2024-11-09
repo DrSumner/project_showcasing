2a2e83f62240824c7221b80b8cb3fceaebe1e42c
DrSumner <157843168+DrSumner@users.noreply.github.com>
2024-11-09 13:01:10 -0500

    polish

--- src/pages/infopages/contact.jsx
+++ src/pages/infopages/contact.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const Contact = () => {
   
     return (
-      <div>
+      <div className="contact-container">
         <h1>Where can you find me? Links down below</h1>
-        <ul>
-          <li> <a href="https://www.instagram.com/tre_productions/">Instagram</a></li>
-          <li> <a href="https://x.com/TreproductionsG">Twitter</a></li>
-          <li> <a href="https://www.linkedin.com/in/latrell-sumner-code/">LinkedIn</a></li>
-          <li> <a href="https://www.youtube.com/@treproductions9522">Youtube</a></li>
+        <ul className="contact-list" >
+          <li> <a href="https://www.instagram.com/tre_productions/" target="_blank" rel="noopener noreferrer" >Instagram</a></li>
+          <li> <a href="https://x.com/TreproductionsG" target="_blank" rel="noopener noreferrer">Twitter</a></li>
+          <li> <a href="https://www.linkedin.com/in/latrell-sumner-code/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+          <li> <a href="https://www.youtube.com/@treproductions9522" target="_blank" rel="noopener noreferrer">Youtube</a></li>
         </ul>
       </div>
     );
