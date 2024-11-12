d7d4308b7716ceba9fe0837d338be95af80a2993
DrSumner <157843168+DrSumner@users.noreply.github.com>
2024-11-12 13:33:15 -0500

    temporary home page for students

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import calypso from "../media/audio/Calypso (guitair)_1.mp3"
 
 
 const Home = () => {
@@ -10,12 +11,19 @@ const Home = () => {
 
             Select a button to get started!
             </h1>
-            <h2>
+            {/* <h2>
                 click "Tracks" to hear some tracks and watch a video of me creating it.
             </h2>
             <h2>
                 Or click "Info" to here more music I've worked on and my socials.
-            </h2>
+            </h2> */}
+            <h3 style={{color:'green'}} >
+                My old assisnment for researching a culture's music! Mine was "calypso"
+            </h3>
+            <div style={{color:'green'}} >Heres the track: <audio controls >
+            <source src={calypso}/>
+            Your browser does not support the audio element
+            </audio> </div>
         </div>
 
     );
